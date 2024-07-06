a5b724d951b2d0fcfd7face4966a46182f732458
netsamma <netsamma@gmail.com>
2024-07-07 01:33:52 +0200

    Product service variable

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,14 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../interfaces/product';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
-
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
   constructor(private http: HttpClient) {}
-
+  // Prova
   private url = environment.apiUrl;
 
   private searchSubject = new BehaviorSubject<string>('');
