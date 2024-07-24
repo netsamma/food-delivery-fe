45dedcd5914a462ad0ba2bb44ff7f90c33bbb3f8
netsamma <netsamma@gmail.com>
2024-07-24 08:55:12 +0200

    Dashboard - Button - 1

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -4,11 +4,12 @@ import users from '../../data/users';
 import { FilterUserPipe } from '../../pipes/filter-user-pipe';
 import { FormsModule } from '@angular/forms';
 import { ShopFormComponent } from '../shop-form/shop-form.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FilterUserPipe, FormsModule, ShopFormComponent],
+  imports: [FilterUserPipe, FormsModule, ShopFormComponent, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
